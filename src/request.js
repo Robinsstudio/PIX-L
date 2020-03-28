5ebf0995f13d5dd49f09ce8a6910b7df8b592966
Robinsstudio <robin_1997@hotmail.fr>
2020-03-28 17:20:24 +0100

    request docs

--- src/request.js
+++ src/request.js
@@ -1,3 +1,6 @@
+/**
+ * Performs a call to the back-end API with the specified URL and body.
+ */
 export default (url, body) => {
 	return fetch(process.env.PUBLIC_URL + '/api/' + url, {
 		method: 'POST',
