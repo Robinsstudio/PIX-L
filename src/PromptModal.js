2d3fb707f3b5dbd2ceb0aec9111f21a37375fc74
Robinsstudio <robin_1997@hotmail.fr>
2020-04-04 18:05:17 +0200

    PromptModal docs

--- src/PromptModal.js
+++ src/PromptModal.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
 
+/**
+ * This component is a modal which allows the user to type text.
+ */
 class PromptModal extends Component {
 	constructor(props) {
 		super(props);
@@ -8,6 +11,11 @@ class PromptModal extends Component {
 		this.handleKeyDown = this.handleKeyDown.bind(this);
 	}
 
+	/**
+	 * If the Enter key is pressed, then the modal closes with the entered text.
+	 *
+	 * @param {KeyboardEvent} event - the keydown event
+	 */
 	handleKeyDown(event) {
 		const { value } = this.props;
 		if (event.key === 'Enter') {
@@ -15,18 +23,33 @@ class PromptModal extends Component {
 		}
 	}
 
+	/**
+	 * Called if the user presses the Enter key or the 'Validate' button.
+	 *
+	 * @param {string} value - the text typed by the user
+	 */
 	onConfirm(value) {
 		const { hide, promise: { resolve } } = this.props;
 		resolve(value);
 		hide();
 	}
 
+	/**
+	 * Called if the user pressed the 'Cancel' button or dismisses the modal.
+	 *
+	 * @param {string} value - the text typed by the user
+	 */
 	onCancel(value) {
 		const { hide, promise: { reject } } = this.props;
 		reject(value);
 		hide();
 	}
 
+	/**
+	 * Automatically focuses the input when the modal is opened.
+	 *
+	 * @param {HTMLInputElement} input - the input element to focus
+	 */
 	focus(input) {
 		setTimeout(() => {
 			if (input) {
@@ -35,6 +58,9 @@ class PromptModal extends Component {
 		}, 0);
 	}
 
+	/**
+	 * Renders the PromptModal.
+	 */
 	render() {
 		const { open, title, placeholder, value, update } = this.props;
 
